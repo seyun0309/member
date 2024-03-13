4f3f2a2bbe2fa0a4d24d5472afa24fbb75df7d74
seyun kim <>
2024-03-13 16:24:00 +0900

    초기세팅

---
+++ src/main/java/com/example/member0/Member0Application.java
@@ -0,0 +1,13 @@
+package com.example.member0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Member0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Member0Application.class, args);
+	}
+
+}
