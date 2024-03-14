167ac96cd8284d70d408fab8d65527321ea4158c
seyun kim <>
2024-03-14 18:45:58 +0900

    [ADD] 로그인, 회원가입, 회원 목록 기능을 처리하는 MemberController 추가

---
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -0,0 +1,60 @@
+package com.example.member.controller;
+
+import com.example.member.dto.MemberDTO;
+import com.example.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    //생성자 주입
+    private final MemberService memberService;
+
+    //회원가입 페이지 요청
+    @GetMapping("/member/save")
+    public String saveForm(){
+        return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if(loginResult != null){
+            //로그인 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            //로그인 실패
+            return "login";
+        }
+    }
+
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // html로 가져갈 데이터가 있다면 model 사용
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
+    }
+}
