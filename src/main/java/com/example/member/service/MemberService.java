dbad1aa575cf2caeeb44c21ecba4c65e5edaf9d0
seyun kim <>
2024-03-14 22:20:33 +0900

    [ADD] 회원 정소 수정 서비스 추가

--- src/main/java/com/example/member/service/MemberService.java
+++ src/main/java/com/example/member/service/MemberService.java
@@ -66,4 +66,17 @@ public class MemberService {
             return null;
         }
     }
+
+    public MemberDTO updateForm(String myEmail) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(myEmail);
+        if(optionalMemberEntity.isPresent()) {
+            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public void update(MemberDTO memberDTO) {
+        memberRepository.save(MemberEntity.toUpdateMemberentity(memberDTO));
+    }
 }
