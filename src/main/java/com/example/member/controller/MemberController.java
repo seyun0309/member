161f96e0df46fc4a1e7edcd0d3cee86035e4f110
seyun kim <>
2024-03-14 21:36:27 +0900

    [ADD] 회원 상세 정보 컨트롤러 추가

--- src/main/java/com/example/member/controller/MemberController.java
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -56,5 +57,12 @@ public class MemberController {
         // html로 가져갈 데이터가 있다면 model 사용
         model.addAttribute("memberList", memberDTOList);
         return "list";
+    }
+
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "detail";
     }
 }
